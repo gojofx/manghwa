28e6678caef006cbcddc8a5809b1d0e2c92bf668
Johan_londono <johan.londono@carvajal.com>
2023-06-17 01:42:07 -0500

    feat: initial commit

---
+++ src/modules/anz-manga/routes.ts
@@ -0,0 +1,8 @@
+import express from "express";
+export const router = express.Router();
+import * as controller from './controllers'
+import { exceptionHandler } from "../../util/exceptionHandler";
+
+router.get('/findByName', exceptionHandler(controller.findByName))
+router.get('/findChaptersByName', exceptionHandler(controller.findChaptersByName))
+router.get('/findChapter', exceptionHandler(controller.findChapter))
